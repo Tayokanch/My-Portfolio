1e3953c1f8a4c30c49eef8e8c4904f56b10738f9
Tayokanch <tayoquadri11@gmail.com>
2024-01-25 20:00:40 +0000

    still fixing the project 4 issue

--- src/MainComponents/Portfolio.jsx
+++ src/MainComponents/Portfolio.jsx
@@ -3,7 +3,7 @@ import "./Portfolio.css";
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/Project3.png";
-import project4 from "../assets/project4.png";
+import project4 from "../assets/project3.png";
 
 import { useNavigate } from "react-router-dom";
 
